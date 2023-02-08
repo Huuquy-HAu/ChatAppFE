44864021aa4da2523bea904cf27723cf0b8c4c2b
Huuquy-HAu <quyhuu19@gmail.com>
2023-02-08 15:01:52 +0700

    Commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
 import "antd/dist/reset.css";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from '../components/Home/Home';
+import SignIn from '../components/Login/SignIn';
+import SignUp from '../components/Login/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          
+          <Route path='/' element={<Home/>}/>
+          <Route path='/sign-in' element={<SignIn/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
       </Routes>
     </BrowserRouter>
   );
