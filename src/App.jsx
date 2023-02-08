886f5275e5e677ad22a9de6d4390639c6956fb65
Huuquy-HAu <quyhuu19@gmail.com>
2023-02-08 15:23:45 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
 import "antd/dist/reset.css";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from '../components/Home/Home';
-import SignIn from '../components/Login/SignIn';
-import SignUp from '../components/Login/SignUp';
+import Home from './components/Home/Home';
+import SignIn from './components/Login/SignIn';
+import SignUp from './components/Login/SignUp';
+import Admin from './components/Admin/Admin';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/sign-in' element={<SignIn/>}/>
           <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path='/admin' element={<Admin/>}/>
       </Routes>
     </BrowserRouter>
   );
