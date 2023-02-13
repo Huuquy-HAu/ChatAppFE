832f576aa8892a586bdd9727ce534bb1150efee3
zzNe0zz <zzhoangzz1995@gmail.com>
2023-02-13 15:22:40 +0700

    hoang

--- src/components/Main/Componem/ChatMain.jsx
+++ src/components/Main/Componem/ChatMain.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
-
+import {PlusCircleOutlined } from "@ant-design/icons"
 function ChatMain() {
   return (
-    <div className='chatMain'>ChatMain</div>
+    <div className='chatMain'>
+      <div className='chatMain-top'>
+        <div className='chatMain-top-left'>
+           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTavONlrqeMyP2frM69zYo8jHXpMhJhoW-WOw&usqp=CAU" alt="" />
+         <div className='chatMain-top-left-text'><span>Name</span></div>
+        </div>
+        <div className='chatMain-top-right'>
+         <i class="fa-solid fa-ellipsis"></i>
+        </div>
+      </div>
+      <div className='chatMain-main'>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+          <p>1</p>
+      </div>
+      <div className='chatMain-bot'>
+        <div className='chatMain-bot-icon'>
+           <i class="fa-regular fa-image"></i>
+           <div><PlusCircleOutlined /></div>
+        </div>
+        <div className='chatMain-bot-iput'>
+          <input type="text" placeholder='Aa'/>
+          <i class="fa-regular fa-face-smile-beam"></i>
+        </div>
+        <div><i class="fa-regular fa-thumbs-up"></i></div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Main/Componem/Infor.jsx
+++ src/components/Main/Componem/Infor.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 function Infor() {
   return (
-    <div className='infor'>Infor</div>
+    <div className='infor'>
+      <div className='infor-top'>
+          <div className='infor-top-img'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2NhI8N6r4uMLZQu-cgx6ST0kmx3yyYT1WCQ&usqp=CAU" alt="" /></div>
+          <h3>Name</h3>
+      </div>
+      <div className='infor-bot'>
+          <div className='infor-bot-menu'>
+              <div className='infor-bot-menu-list'>
+                <i class="fa-solid fa-circle-user"></i>
+                <p>Trang cá nhân</p>
+              </div>
+          </div>
+          <div className='infor-bot-menu'>
+          <div className='infor-bot-menu-list'>
+                <i class="fa-solid fa-circle-user"></i>
+                <p>Trang cá nhân</p>
+              </div>
+          </div>
+          <div className='infor-bot-menu'>
+          <div className='infor-bot-menu-list'>
+                <i class="fa-solid fa-circle-user"></i>
+                <p>Trang cá nhân</p>
+              </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
