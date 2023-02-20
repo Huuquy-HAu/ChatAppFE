df391c191177d2aea42a7f3465334f30021ac608
zzNe0zz <zzhoangzz1995@gmail.com>
2023-02-20 17:52:42 +0700

    hoang

--- src/components/Main/Componem/ChatMain.jsx
+++ src/components/Main/Componem/ChatMain.jsx
@@ -1,7 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState,useRef  } from 'react'
 import {PlusCircleOutlined } from "@ant-design/icons"
+import Picker from '@emoji-mart/react'
+import data from '@emoji-mart/data'
 function ChatMain() {
   const [show,setShow] = useState(false)
+  const [controlerEM,setControlerEM] = useState(false)
+  const [chosenEmoji, setChosenEmoji] = useState(null);
+  const dataCHat = [1,2,3,4,5]
+  const [dataClone,setDataClone] = useState(dataCHat)
+  const inputElement = useRef();
+  function showEM (){
+    setControlerEM(true)
+  }
+  function clickEmoj (even){
+    setDataClone([...dataClone,even.native])
+    setControlerEM(false)
+  }
+  function updateScroll (){
+    var element = document.querySelector(".chatMain-main");
+    console.log(element.scrollHeight );
+    element.scrollTop = element.scrollHeight;
+  }
+  function chat (e){
+  if(e.keyCode === 13){
+    setDataClone([...dataClone,inputElement.current.value])
+    inputElement.current.value = ""
+    inputElement.current.focus()
+  }
+  }
   function showInfor (){
     let taget = document.querySelector(".homePage-right")
     let target =document.querySelector(".infor")
@@ -17,7 +43,7 @@ function ChatMain() {
     }
   }
   return (
-    <div className='chatMain'>
+    <div className='chatMain' >
       <div className='chatMain-top'>
         <div className='chatMain-top-left'>
            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTavONlrqeMyP2frM69zYo8jHXpMhJhoW-WOw&usqp=CAU" alt="" />
@@ -27,39 +53,26 @@ function ChatMain() {
          <i class="fa-solid fa-ellipsis"></i>
         </div>
       </div>
-      <div className='chatMain-main'>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
-          <p>1</p>
+      <div className='chatMain-main' onClick={()=> setControlerEM(false)} >
+          {
+            dataClone.map((value,index)=> <p key={index}>{value}</p>)
+          }
       </div>
       <div className='chatMain-bot'>
         <div className='chatMain-bot-icon'>
-           <i class="fa-regular fa-image"></i>
+           <i className="fa-regular fa-image"></i>
            <div><PlusCircleOutlined /></div>
         </div>
         <div className='chatMain-bot-iput'>
-          <input type="text" placeholder='Aa'/>
-          <i class="fa-regular fa-face-smile-beam"></i>
+          <input type="text" placeholder='Aa' onKeyUp={chat} ref={inputElement} onChange={updateScroll}/>
+          <div className='chatMain-bot-iput-emoj'>
+              <i className="fa-regular fa-face-smile-beam" onClick={showEM}></i>
+              {controlerEM ? <div className='emoj'><Picker  data={data} onEmojiSelect = {clickEmoj}/></div>:""}
+          </div>
         </div>
-        <div><i class="fa-regular fa-thumbs-up"></i></div>
+        <div ><i className="fa-regular fa-thumbs-up"></i>
+        
+        </div>
       </div>
     </div>
   )
---
+++ src/components/Main/Friend/Addfriend.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Addfriend() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Addfriend
